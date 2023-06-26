e85a5f7c11b601af80e337479b263c6caf078982
Ilya Sgibnev <sgibnil@gmail.com>
2023-06-26 16:33:24 +0300

    start2

--- ConsoleApplication1/ConsoleApplication1/Code.cpp
+++ ConsoleApplication1/ConsoleApplication1/Code.cpp
@@ -6,12 +6,9 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-eDirection dir;
 
 void Setup() {
 	gameOver = false;
-	dir = STOP;
 	x = width / 2;
 	y = height / 2;
 	fruitX = rand() % width;
@@ -46,10 +43,8 @@ void Logic() {
 
 int main() {
 	Setup();
-	while (!gameOver) {
-		Draw();
-		Input();
-		Logic();
-	}
+	Draw();
+	Input();
+	Logic();
 	return 0;
 }
